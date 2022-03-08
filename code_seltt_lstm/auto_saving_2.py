f4210124d4ee2d67a1fee06aa5cccf66495df870
limejuicyj <yj.shinn@gmail.com>
2022-03-08 17:14:47 +0900

    auto saving

--- code_seltt_lstm/auto_saving_2.py
+++ code_seltt_lstm/auto_saving_2.py
@@ -7,16 +7,17 @@ import datetime
 # 한 폴더내에 반드시 epoch이 동일한 파일만 있어야 에러 안남
 
 ##### Folder and File list #####
-INPUT_FOLDER = r'./results/'
+INPUT_FOLDER = r'/media/data1/sel_ttlstm/mnist_results/default/'
+SAVING_FOLDER = r'./results/'
 # INPUT_FOLDER = r'./results/epoch10/'
 # INPUT_FOLDER = r'./results/epoch5/'
-INPUT_FILE = 'cifar10_'       # data name
-# INPUT_FILE = ''             # data name
+# INPUT_FILE = 'cifar10_'       # data name
+INPUT_FILE = ''             # data name
 # INPUT_FILE = 'lstm_'             # data name
 all_files = glob.glob(INPUT_FOLDER + INPUT_FILE + '*.txt')
 
 ##### Printing Options #####
-saving_flag = 0                     # Save to csv : 1, without saving : 0
+saving_flag = 1                     # Save to csv : 1, without saving : 0
 saving_epoch = 3                    # print X times of epochs. if 1: print all / if 5: 5,10,15...
 
 
@@ -51,31 +52,50 @@ for i in all_files:
                     df.append(y)
     # print("---------------------------- ")
     print("df:",df)
-    print("df:",df[0])
+    print("df:",df[1])
 
 
 
     #### F1."File name" #####
-    df_main = pd.DataFrame({"File name":[df[0]]})
+    df_main = pd.DataFrame({"File name":[i]})
 
     ##### F2. "Namespace" #####
-    df[1] = df[1].replace(" ", "")
-    name_split = df[1].split(",")
-    name_split[0] = name_split[0].split("(")[1]
-    name_split[len(name_split)-1] = name_split[len(name_split)-1].split(")")[0]
-    # print("\nAfter split:", name_split)
+    file_name = i.split("/")
+    file_name = file_name[len(file_name)-1].split("_")
+    print("file name split :", file_name)
+    f_dataset = file_name[0]
+    print("f_dataset :", f_dataset)
+    idx = 2
+    if f_dataset == "lstm":
+        idx = idx-1
+    f_input_size = file_name[idx].split("in")[1]
+    f_hidden_size = file_name[idx+5].split("h")[1]
+    f_mode = file_name[idx+1]
+    f_n_layer = (file_name[idx+2].split("(")[0]).split("n")[1]
+    f_n_cores = file_name[idx + 3].split("ncores")[1]
+    f_tt_ranks = file_name[idx + 4].split("r")[1]
+    f_n_front_layer = ""
+    if f_mode == "basic":
+        f_n_cores = ""
+        f_tt_ranks = ""
+    elif f_mode == "sel":
+        f_n_front_layer = (file_name[idx+2].split("(")[1]).split("+")[0]
+    print("f_mode :{}".format(f_mode))
+    print("f_n_layer :{} / f_n_front_layer :{} ".format(f_n_layer,f_n_front_layer))
+    print("f_n_cores :{} / f_tt_ranks :{} ".format(f_n_cores,f_tt_ranks))
 
-    cols=[]
-    vals=[]
-    for i in range(0,len(name_split)):
-        name_split[i] = name_split[i].split("=")
-        cols.append(name_split[i][0])
-        vals.append(name_split[i][1])
-        # print("cols: ",cols)
-        # print("vals: ",vals)
-    df_tmp = pd.DataFrame(vals).transpose()
-    df_tmp.columns = cols
-    # print("df_tmp : ", df_tmp)
+    lr_tmp = file_name[idx+6].split("lr")
+    if lr_tmp[0] == "":
+        f_lr = lr_tmp[1]
+        f_epoch = file_name[idx + 7].split("ep")[1]
+    else:
+        f_lr = 0.01
+        f_epoch = (file_name[idx + 6].split("ep")[1]).split(".")[0]
+    print("f_lr :{} / f_epoch :{} ".format(f_lr,f_epoch))
+
+    df_tmp = pd.DataFrame({"dataset": [f_dataset], "input_size": [f_input_size], "hidden_size": [f_hidden_size],
+                            "epochs":[f_epoch], "mode":[f_mode], "n_layers":[f_n_layer], "n_front_layers":[f_n_front_layer],
+                            "ncores":[f_n_cores], "ttrank":[f_tt_ranks], "lr":[f_lr]})
 
     df_main = pd.concat([df_main, df_tmp], axis=1)
 
@@ -86,9 +106,9 @@ for i in all_files:
     total_param_num = total_split[len(total_split)-1].split("\n")[0]
 
     df_main["#parameters"] = [total_param_num]
-    print("df_main :",df_main)
+    print("df_main" , df_main)
     num_cols = len(df_main.columns)
-
+    print("num_cols",num_cols)
 
 
     ##### F6. "Average loss (Accuracy) and Runtime" #####
@@ -112,14 +132,12 @@ for i in all_files:
 
 ##### #####
 print("FINAL_draft : \n", df_final)       # including all information
-print("FINAL_draft len : \n", len(df_final.columns) )      # including all information
 
 
 
 
 ##### Col Extraction #####
-# col_1 = ['mode', 'File name', 'n_layers', 'n_front_layers', 'ncores', 'ttrank', 'epochs', 'lr', 'batch_size', 'hidden_size']
-col_1 = ['mode', 'n_layers', 'n_front_layers', 'ncores', 'ttrank', 'epochs', 'lr', 'batch_size', 'hidden_size', '#parameters']
+col_1 = ['dataset', 'input_size', 'mode', 'n_layers', 'n_front_layers', 'ncores', 'ttrank', 'epochs', 'lr', 'hidden_size', '#parameters']
 
 
 col_tmp = df_final.columns[num_cols:].to_list()     #ALL loss and acc
